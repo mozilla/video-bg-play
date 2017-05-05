7412889ab62fc970a41339fb1f0050db5a4bd609
Timothy Guan-tin Chien <timdream@gmail.com>
2017-05-05 17:00:42 +0800

    Also work with fullscreen API

--- video-bg-play-content.js
+++ video-bg-play-content.js
@@ -1,7 +1,18 @@
 'use strict';
 
+// Page Visibility API
 Object.defineProperties(document.wrappedJSObject,
   { 'hidden': {value: false}, 'visibilityState': {value: 'visible'} });
 
 document.addEventListener(
   'visibilitychange', evt => evt.stopImmediatePropagation(), true);
+
+// Fullscreen API
+document.addEventListener('fullscreenchange', evt => {
+  Object.defineProperties(document.wrappedJSObject,
+    { 'fullscreenEnabled': {value: true},
+      'fullscreen': {value: true},
+      'fullscreenElement': {value: document.fullscreenElement.wrappedJSObject}});
+  document.addEventListener(
+    'fullscreenchange', evt => evt.stopImmediatePropagation(), true);
+}, { capture: true, once: true });
