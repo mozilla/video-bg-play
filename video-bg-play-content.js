67d0cb7c4d905f9b68050a8bcf52ed883e1c64de
Jonathan García <JoGarWeb@users.noreply.github.com>
2018-12-18 22:14:51 -0600

    Update video-bg-play-content.js
    Deleting this part of the code solve this problem: https://github.com/mozilla/video-bg-play/issues/8 
    This problem appears since Firefox 64+

--- video-bg-play-content.js
+++ video-bg-play-content.js
@@ -6,13 +6,3 @@ Object.defineProperties(document.wrappedJSObject,
 
 window.addEventListener(
   'visibilitychange', evt => evt.stopImmediatePropagation(), true);
-
-// Fullscreen API
-window.addEventListener('fullscreenchange', evt => {
-  Object.defineProperties(document.wrappedJSObject,
-    { 'fullscreenEnabled': {value: true},
-      'fullscreen': {value: true},
-      'fullscreenElement': {value: document.fullscreenElement.wrappedJSObject}});
-  window.addEventListener(
-    'fullscreenchange', evt => evt.stopImmediatePropagation(), true);
-}, { capture: true, once: true });
