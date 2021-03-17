29fa03b9d6ecccc890aa7b361586067dee9a54a4
Jan Henning <jh+bitbucket@buttercookie.de>
2021-03-17 16:05:56 +0100

    Try different method of preventing the inactivity warning from triggering

--- video-bg-play-content.js
+++ video-bg-play-content.js
@@ -25,22 +25,37 @@ if (IS_VIMEO) {
 
 // User activity tracking
 if (IS_YOUTUBE) {
-  const refreshInterval = 5 * 60 * 1000; // every 5 minutes
-  waitForYoutubeLactInit(() => refreshLact(), refreshInterval);
+  loop(pressKey, 60 * 1000, 10 * 1000); // every minute +/- 5 seconds
 }
 
-function waitForYoutubeLactInit(aCallback, aCallbackInterval, aDelay = 1000) {
-  let pageWin = window.wrappedJSObject;
-  if (pageWin.hasOwnProperty('_lact')) {
-    window.setInterval(aCallback, aCallbackInterval);
-  } else {
-    window.setTimeout(() => waitForYoutubeLactInit(aCallback,
-                                                   aCallbackInterval,
-                                                   aDelay * 2),
-                      aDelay);
-  }
+function pressKey() {
+  const keyCodes = [18];
+  let key = keyCodes[getRandomInt(0, keyCodes.length)];
+  sendKeyEvent("keydown", key);
+  sendKeyEvent("keyup", key);
 }
 
-function refreshLact() {
-  window.wrappedJSObject._lact = Date.now();
+function sendKeyEvent (aEvent, aKey) {
+  document.dispatchEvent(new KeyboardEvent(aEvent, {
+    bubbles: true,
+    cancelable: true,
+    keyCode: aKey,
+    which: aKey,
+  }));
+}
+
+function loop(aCallback, aDelay, aJitter) {
+  let jitter = getRandomInt(-aJitter/2, aJitter/2);
+  let delay = Math.max(aDelay + jitter, 0);
+
+  window.setTimeout(() => {
+                      aCallback();
+                      loop(aCallback, aDelay, aJitter);
+                    }, delay);
+}
+
+function getRandomInt(aMin, aMax) {
+  let min = Math.ceil(aMin);
+  let max = Math.floor(aMax);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
